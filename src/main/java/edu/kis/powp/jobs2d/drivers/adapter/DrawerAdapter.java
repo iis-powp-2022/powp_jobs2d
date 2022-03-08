136c48173dc4831257e0e4a68c8c6cc693c1af69
Panzer0 <daniel.pazur@gmail.com>
2022-03-08 22:32:59 +0100

    Renamed myAdapter class and implemented toString

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -8,10 +8,10 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DrawerAdapter() {
 		super();
 	}
 
@@ -33,6 +33,6 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "(" + this.startX + ")" + "(" + this.startY + ")";
 	}
 }
