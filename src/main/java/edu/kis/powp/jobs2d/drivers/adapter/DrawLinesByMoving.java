12b134f019c397781b030ebf774600090fbcd697
Wojciech Farej <228730@edu.p.lodz.pl>
2022-03-09 11:26:51 +0100

    Drawing in internal window now

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawLinesByMoving.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawLinesByMoving.java
@@ -6,6 +6,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -31,8 +32,8 @@ public class DrawLinesByMoving extends DrawPanelController implements Job2dDrive
 
 		startX = x;
 		startY = y;
-
-		drawLine(line);
+		
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
