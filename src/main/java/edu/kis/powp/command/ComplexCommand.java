1aecf82a166f5b6255367a5f594154cfc57ae948
Jakzaj <jakubzajac13@gmail.com>
2022-03-16 11:42:03 +0100

    Lab2

---
+++ src/main/java/edu/kis/powp/command/ComplexCommand.java
@@ -0,0 +1,13 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class ComplexCommand implements DriverCommand {
+
+    DriverCommand driverCommand;
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,14 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand{
+
+    int x;
+    int y;
+
+    @Override
+    public void execute(Job2dDriver job2dDriver) {
+        job2dDriver.operateTo(this.x,this.y);
+    }
+}
