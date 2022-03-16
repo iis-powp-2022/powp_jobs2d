d1f4a6375e6317b144338fdb67805835c393461d
Różycki, Bartosz <bartek.rozycki@icloud.com>
2022-03-16 10:27:20 +0100

    use tabs instead of spaces

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2DToDrawPanelAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2DToDrawPanelAdapter.java
@@ -1,7 +1,5 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
-import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
@@ -12,29 +10,29 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
  */
 public class Job2DToDrawPanelAdapter implements Job2dDriver {
 
-  private int startX = 0, startY = 0;
+	private int startX = 0, startY = 0;
 
-  @Override
-  public void setPosition(int x, int y) {
-    this.startX = x;
-    this.startY = y;
-  }
+	@Override
+	public void setPosition(int x, int y) {
+		this.startX = x;
+		this.startY = y;
+	}
 
-  @Override
-  public void operateTo(int x, int y) {
-    ILine line = LineFactory.getBasicLine();
-    line.setStartCoordinates(this.startX, this.startY);
+	@Override
+	public void operateTo(int x, int y) {
+		ILine line = LineFactory.getBasicLine();
+		line.setStartCoordinates(this.startX, this.startY);
     line.setEndCoordinates(x, y);
 
-    DrawerFeature.getDrawerController().drawLine(line);
-    this.setPosition(x, y);
-  }
+		DrawerFeature.getDrawerController().drawLine(line);
+		this.setPosition(x, y);
+	}
 
-  @Override
-  public String toString() {
-    return "MyAdapter{" +
-        "startX=" + startX +
-        ", startY=" + startY +
-        '}';
-  }
+	@Override
+	public String toString() {
+		return "MyAdapter{" +
+				"startX=" + startX +
+				", startY=" + startY +
+				'}';
+	}
 }
--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -7,21 +7,21 @@ import java.awt.event.ActionListener;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-  public static String FIGURE_JOE_1 = "Figure Joe 1";
+	public static String FIGURE_JOE_1 = "Figure Joe 1";
   public static String FIGURE_JOE_2 = "Figure Joe 2";
 
-  private DriverManager driverManager;
+	private DriverManager driverManager;
 
-  public SelectTestFigureOptionListener(DriverManager driverManager) {
-    this.driverManager = driverManager;
-  }
+	public SelectTestFigureOptionListener(DriverManager driverManager) {
+		this.driverManager = driverManager;
+	}
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    if (FIGURE_JOE_1.equals(e.getActionCommand())) {
-      FiguresJoe.figureScript1(driverManager.getCurrentDriver());
-    } else if (FIGURE_JOE_2.equals(e.getActionCommand())) {
-      FiguresJoe.figureScript2(driverManager.getCurrentDriver());
-    }
-  }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (FIGURE_JOE_1.equals(e.getActionCommand())) {
+			FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		} else if (FIGURE_JOE_2.equals(e.getActionCommand())) {
+			FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+		}
+	}
 }
