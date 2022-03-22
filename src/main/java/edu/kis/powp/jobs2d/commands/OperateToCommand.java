daabb9a656d74ee091871a0e220cae5332ed99b5
Konrad Basta <konrad.basta@onet.com.pl>
2022-03-22 12:47:46 +0100

    Added DeiverCommand Interface

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -0,0 +1,7 @@
+package edu.kis.powp.jobs2d.commands;
+
+public class OperateToCommand {
+	private int x;
+	private int y;
+}
+
---
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d.commands;
+
+public class SetPositionCommand {
+	private int x;
+	private int y;
+}
