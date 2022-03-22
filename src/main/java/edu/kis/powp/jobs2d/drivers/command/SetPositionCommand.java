169e0d2c7ad52e89e844703da31760584cb68c36
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-22 13:18:37 +0100

    Implementing concrete operations in classes

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -1,6 +1,33 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
-public class SetPositionCommand {
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand{
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    public SetPositionCommand(int x, int y, Job2dDriver driver) {
+        this.x = x;
+        this.y = y;
+        this.driver = driver;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
+
+    @Override
+    public void execute() {
+        this.driver.setPosition(this.x,this.y);
+    }
+
 }
