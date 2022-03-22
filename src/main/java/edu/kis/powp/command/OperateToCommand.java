46c27f2f678a75a8a6852a967a3fce8c6c0be8c1
rafiti12 <rafiti12@o2.pl>
2022-03-22 13:34:31 +0100

    Command package Added a new package with 1 interface and 2 empty classes

---
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.command;
+
+public class OperateToCommand implements DriverCommand {
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void setPosition(int i, int i1) {
+
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,21 @@
+package edu.kis.powp.command;
+
+public class SetPositionCommand implements DriverCommand {
+    int x;
+    int y;
+
+    @Override
+    public void setPosition(int i, int i1) {
+
+    }
+
+    @Override
+    public void operateTo(int i, int i1) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
