f55bdbda36bf40f8213ff682c663ae47e0ddaea1
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-22 12:44:51 +0100

    Added new package

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class OperateToCommand {
+    private int x;
+    private int y;
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand {
+    private int x;
+    private int y;
+}
