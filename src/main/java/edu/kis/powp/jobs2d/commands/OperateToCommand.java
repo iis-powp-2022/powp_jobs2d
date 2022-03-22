d4b69a3fe6c98e8ce8501b7fcfc423e2b97ee2b4
Konrad Basta <konrad.basta@onet.com.pl>
2022-03-22 12:59:09 +0100

    Implemented Commands

--- src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/OperateToCommand.java
@@ -1,7 +1,25 @@
 package edu.kis.powp.jobs2d.commands;
 
-public class OperateToCommand {
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class OperateToCommand implements DriverCommand{
 	private int x;
 	private int y;
+
+	Job2dDriver driver;
+
+	public OperateToCommand(Job2dDriver driver) {
+		this.driver = driver;
+	}
+
+	public void SetPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public void execute() {
+		driver.operateTo(x, y);
+	}
 }
 
--- src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -1,6 +1,24 @@
 package edu.kis.powp.jobs2d.commands;
 
-public class SetPositionCommand {
-	private int x;
-	private int y;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand{
+	private int x = 0;
+	private int y = 0;
+
+	Job2dDriver driver;
+
+	public SetPositionCommand(Job2dDriver driver) {
+		this.driver = driver;
+	}
+
+	public void SetPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	@Override
+	public void execute() {
+		driver.setPosition(x, y);
+	}
 }
