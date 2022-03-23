16d1bc5870209f6fee3aea8f526f8c20082c321a
FranciszekKarwowski <96189378+FranciszekKarwowski@users.noreply.github.com>
2022-03-23 19:50:50 +0100

    Added triangle test

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import edu.kis.powp.command.SquareFactory;
+import edu.kis.powp.command.TriangleFactory;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
@@ -20,5 +21,6 @@ public class SelectTestFigureOptionListener implements ActionListener {
 		if(e.getActionCommand().equals("Figure Joe 1")) FiguresJoe.figureScript1(driverManager.getCurrentDriver());
 		else if(e.getActionCommand().equals("Figure Joe 2")) FiguresJoe.figureScript2(driverManager.getCurrentDriver());
 		else if(e.getActionCommand().equals("Square")) new SquareFactory(driverManager.getCurrentDriver()).createComplexCommand().execute();
+		else if(e.getActionCommand().equals("Triangle")) new TriangleFactory(driverManager.getCurrentDriver()).createComplexCommand().execute();
 	}
 }
