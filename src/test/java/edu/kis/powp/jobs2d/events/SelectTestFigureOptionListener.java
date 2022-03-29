e6dcf6f5b8c7976bd6d80030ed74489e3f9852f6
backs <26674182+Backss4@users.noreply.github.com>
2022-03-29 23:15:44 +0200

    Added FiguresJoe::figureScript2 test

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -2,20 +2,29 @@ package edu.kis.powp.jobs2d.events;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.function.Consumer;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
-	private DriverManager driverManager;
+	private final DriverManager driverManager;
+
+	private final HashMap<String, Consumer<Job2dDriver>> tests = new HashMap<>();
 
 	public SelectTestFigureOptionListener(DriverManager driverManager) {
 		this.driverManager = driverManager;
+		this.tests.put("Figure Joe 1", FiguresJoe::figureScript1);
+		this.tests.put("Figure Joe 2", FiguresJoe::figureScript2);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		if(tests.containsKey(e.getActionCommand())) {
+			tests.get(e.getActionCommand()).accept(driverManager.getCurrentDriver());
+		}
 	}
 }
