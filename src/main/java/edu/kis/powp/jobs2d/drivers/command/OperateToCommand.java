3ce048d591c81a00d879e724c899b8e9c8c821d7
Panzer0 <daniel.pazur@gmail.com>
2022-04-01 03:18:32 +0200

    Added unfinished command classes

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class OperateToCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,12 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x;
+    private int y;
+
+    @Override
+    public void execute() {
+
+    }
+
+}
