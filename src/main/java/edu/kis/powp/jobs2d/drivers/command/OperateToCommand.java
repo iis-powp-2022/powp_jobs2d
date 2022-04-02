70470b21ca7d78c82edcab9d4b1755834ad4dbcf
Daniel Wieczorek <daniel.pazur@gmail.com>
2022-04-02 20:04:41 +0200

    Implemented functional command classes

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,11 +1,38 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Job2dDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
 
     @Override
     public void execute() {
-
+        this.driver.operateTo(this.x, this.y);
     }
 }
--- src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -1,12 +1,39 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
     private int x;
     private int y;
+    private Job2dDriver driver;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Job2dDriver getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
 
     @Override
     public void execute() {
-
+        this.driver.setPosition(this.x, this.y);
     }
 
 }
