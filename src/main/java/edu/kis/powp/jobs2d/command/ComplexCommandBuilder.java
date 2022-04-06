0933d064d3fb5e9c46029565c0da25e929d8b782
backs <26674182+Backss4@users.noreply.github.com>
2022-04-06 17:58:56 +0200

    Added ComplexCommandBuilder

--- src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandBuilder.java
+++ src/main/java/edu/kis/powp/jobs2d/command/ComplexCommandBuilder.java
@@ -1,2 +1,24 @@
-package edu.kis.powp.jobs2d.command;public class ComplexCommandBuilder {
+package edu.kis.powp.jobs2d.command;
+
+public class ComplexCommandBuilder {
+	private ComplexCommandBuilder() {}
+
+	public static DriverCommand getRectangle() {
+		ComplexCommand command = new ComplexCommand();
+		command.addCommand(new SetPositionCommand(-50, -100));
+		command.addCommand(new OperateToCommand(-50, 100));
+		command.addCommand(new OperateToCommand(50, 100));
+		command.addCommand(new OperateToCommand(50, -100));
+		command.addCommand(new OperateToCommand(-50, -100));
+		return command;
+	}
+
+	public static DriverCommand getTriangle() {
+		ComplexCommand command = new ComplexCommand();
+		command.addCommand(new SetPositionCommand(-50, -50));
+		command.addCommand(new OperateToCommand(-75, 50));
+		command.addCommand(new OperateToCommand(75, 50));
+		command.addCommand(new OperateToCommand(-50, -50));
+		return command;
+	}
 }
