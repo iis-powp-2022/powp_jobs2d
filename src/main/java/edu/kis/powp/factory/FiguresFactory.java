8cfcdcdf46d1bcaf102bfdc7376d40efcca19dba
Karol Profic <karolprofic@protonmail.com>
2022-04-06 03:03:10 +0200

    Usunięcie niepotrzebnych importów

--- src/main/java/edu/kis/powp/factory/FiguresFactory.java
+++ src/main/java/edu/kis/powp/factory/FiguresFactory.java
@@ -1,10 +1,8 @@
 package edu.kis.powp.factory;
-
+import edu.kis.powp.command.ComplexCommand;
 import edu.kis.powp.command.DriverCommand;
 import edu.kis.powp.command.OperateToCommand;
 import edu.kis.powp.command.SetPositionCommand;
-import edu.kis.powp.composite.ComplexCommand;
-
 import java.util.ArrayList;
 import java.util.List;
 
