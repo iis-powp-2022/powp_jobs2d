88c1b47bd52f6667c9463bbc4ae8446c0ff4e33f
Karol Profic <karolprofic@protonmail.com>
2022-04-06 02:54:31 +0200

    Dodanie fabryki

---
+++ src/main/java/edu/kis/powp/factory/FiguresFactory.java
@@ -0,0 +1,41 @@
+package edu.kis.powp.factory;
+
+import edu.kis.powp.command.DriverCommand;
+import edu.kis.powp.command.OperateToCommand;
+import edu.kis.powp.command.SetPositionCommand;
+import edu.kis.powp.composite.ComplexCommand;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FiguresFactory {
+    public static ComplexCommand getRectangleComplexCommand() {
+        List<DriverCommand> commandList = new ArrayList<>();
+        commandList.add(new SetPositionCommand(-150, -100));
+        commandList.add(new OperateToCommand(150, -100));
+        commandList.add(new OperateToCommand(150, 100));
+        commandList.add(new OperateToCommand(-150, 100));
+        commandList.add(new OperateToCommand(-150, -100));
+        return new ComplexCommand(commandList);
+    }
+
+    public static ComplexCommand getSquareComplexCommand() {
+        List<DriverCommand> commandList = new ArrayList<>();
+        commandList.add(new SetPositionCommand(-100, -100));
+        commandList.add(new OperateToCommand(100, -100));
+        commandList.add(new OperateToCommand(100, 100));
+        commandList.add(new OperateToCommand(-100, 100));
+        commandList.add(new OperateToCommand(-100, -100));
+        return new ComplexCommand(commandList);
+    }
+
+    public static ComplexCommand getTriangleComplexCommand() {
+        List<DriverCommand> commandList = new ArrayList<>();
+        commandList.add(new SetPositionCommand(0, -125));
+        commandList.add(new OperateToCommand(160, 125));
+        commandList.add(new OperateToCommand(-160, 125));
+        commandList.add(new OperateToCommand(0, -125));
+        return new ComplexCommand(commandList);
+    }
+
+}
