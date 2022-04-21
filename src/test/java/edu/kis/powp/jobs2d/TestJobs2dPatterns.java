f3d6311656343008344e97e162b2cf48d35cb5c0
MateuszMatusiak <mateusz.matusiak000@gmail.com>
2022-04-21 21:42:40 +0200

    lab 1 - 3.3

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.MyAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectSecondFigureListener;
@@ -44,6 +45,9 @@ public class TestJobs2dPatterns {
 
         Job2dDriver testDriver = new MyAdapter();
         DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+        Job2dDriver lineDriver = new LineDrawerAdapter();
+        DriverFeature.addDriver("Line", lineDriver);
 
         DriverFeature.updateDriverInfo();
     }
