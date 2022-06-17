ffbfdd0869bd8a204d6bedb4a010ed11964dd9d0
Piotr Bednarek <specjunior@gmail.com>
2022-06-17 13:45:10 +0200

    Changed Drover to driver, Removed unnecessary inheritance

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -8,7 +8,7 @@ import java.util.logging.Logger;
 import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.powp.appbase.Application;
-import edu.kis.powp.jobs2d.drivers.adapter.DrawerDroverAdapter;
+import edu.kis.powp.jobs2d.drivers.adapter.DrawerDriverAdapter;
 import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
 import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
@@ -43,7 +43,7 @@ public class TestJobs2dPatterns {
 		DriverFeature.addDriver("Logger Driver", loggerDriver);
 		DriverFeature.getDriverManager().setCurrentDriver(loggerDriver);
 
-		Job2dDriver testDriver = new DrawerDroverAdapter();
+		Job2dDriver testDriver = new DrawerDriverAdapter();
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
 
 		Job2dDriver testSpecialDriver = new LineDrawerAdapter();
